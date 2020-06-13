4a5a1c02351ad57ebe8ea86195df6bc2b2cac2ed
Max Hollmann <maxhollmann@gmail.com>
2020-06-13 14:26:18 +0200

    Play around

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,6 +1,6 @@
 <template>
     <Page>
-        <ActionBar>
+        <ActionBar title="Test title">
             <GridLayout width="100%" columns="auto, *">
                 <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
                 <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
@@ -8,12 +8,21 @@
         </ActionBar>
 
         <RadSideDrawer ref="drawer">
-            <StackLayout ~drawerContent backgroundColor="#ffffff">
-                <Label class="drawer-header" text="Drawer"/>
+            <StackLayout ~drawerContent backgroundColor="#2d394e">
+                <Label class="drawer-header" text="Feeds"/>
 
-                <Label class="drawer-item" text="Item 1"/>
-                <Label class="drawer-item" text="Item 2"/>
-                <Label class="drawer-item" text="Item 3"/>
+                <Label class="drawer-item" text="All downloads" />
+                <Label class="drawer-item active" text="Test feed" />
+                <Label class="drawer-item" text="Music" />
+
+                <TextField v-if="showFeedForm"
+                           hint="Create feed" 
+                           color="black" 
+                           backgroundColor="white"
+                           v-model="newFeed.name" />
+                <Label v-if="!showFeedForm" class="show-feed-form" text="+" @tap="showFeedForm = true" />
+                <Button v-else text="Create" @tap="createFeed()"></Button>
+
             </StackLayout>
 
             <GridLayout ~mainContent columns="*" rows="*">
@@ -24,10 +33,31 @@
 </template>
 
 <script lang="ts">
+  // import SideDrawer from 'app/SideDrawer';
+
   export default {
     data() {
       return {
-        msg: 'Hello World!'
+        msg: 'Hello love!',
+        showFeedForm: false,
+        newFeed: {
+            name: '',
+        },
+      }
+    },
+    computed: {
+      addFeedButtonText() {
+          if (this.showFeedForm)
+            return 'Create';
+          else
+            return 'Add feed';
+      }
+    },
+    methods: {
+      createFeed() {
+          console.log(`Creating feed: ${this.newFeed.name}`);
+          this.newFeed.name = '';
+          this.showFeedForm = false;
       }
     }
   }
@@ -35,7 +65,7 @@
 
 <style scoped>
     ActionBar {
-        background-color: #53ba82;
+        background-color: #222b3b;
         color: #ffffff;
     }
 
@@ -51,17 +81,32 @@
         color: #333333;
     }
 
-    .drawer-header {
-        padding: 50 16 16 16;
-        margin-bottom: 16;
-        background-color: #53ba82;
+    .drawer-header, .drawer-item {
+        background-color: #2d394e;
         color: #ffffff;
-        font-size: 24;
+    }
+
+    .drawer-header {
+        padding: 25 16 16 16;
+        /* margin-bottom: 16; */
+        font-size: 16;
+        font-weight: bold;
+        color: #AAA;
     }
 
     .drawer-item {
         padding: 8 16;
-        color: #333333;
         font-size: 16;
+    }
+
+    .drawer-item.active {
+        font-weight: bold;
+    }
+
+    .show-feed-form {
+        text-align: center;
+        font-size: 20;
+        color: #FFFFFF;
+        padding: 8 16;
     }
 </style>
