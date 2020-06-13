91e27a7dde2b2c321f31ba7413884446957322f8
Max Hollmann <maxhollmann@gmail.com>
2020-06-13 17:33:20 +0200

    Move sidedrawer content to its own component

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,112 +1,51 @@
 <template>
-    <Page>
-        <ActionBar title="Test title">
-            <GridLayout width="100%" columns="auto, *">
-                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
-                <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
-            </GridLayout>
-        </ActionBar>
+<Page>
+  <ActionBar title="Test title">
+    <GridLayout width="100%" columns="auto, *">
+      <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
+      <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
+    </GridLayout>
+  </ActionBar>
 
-        <RadSideDrawer ref="drawer">
-            <StackLayout ~drawerContent backgroundColor="#2d394e">
-                <Label class="drawer-header" text="Feeds"/>
-
-                <Label class="drawer-item" text="All downloads" />
-                <Label class="drawer-item active" text="Test feed" />
-                <Label class="drawer-item" text="Music" />
-
-                <TextField v-if="showFeedForm"
-                           hint="Create feed" 
-                           color="black" 
-                           backgroundColor="white"
-                           v-model="newFeed.name" />
-                <Label v-if="!showFeedForm" class="show-feed-form" text="+" @tap="showFeedForm = true" />
-                <Button v-else text="Create" @tap="createFeed()"></Button>
-
-            </StackLayout>
-
-            <GridLayout ~mainContent columns="*" rows="*">
-                <Label class="message" :text="msg" col="0" row="0"/>
-            </GridLayout>
-        </RadSideDrawer>
-    </Page>
+  <RadSideDrawer ref="drawer">
+    <SideDrawer ~drawerContent></SideDrawer>
+    <GridLayout ~mainContent columns="*" rows="*">
+      <Label class="message" :text="msg" col="0" row="0"/>
+    </GridLayout>
+  </RadSideDrawer>
+</Page>
 </template>
 
 <script lang="ts">
-  // import SideDrawer from 'app/SideDrawer';
+import SideDrawer from '~/components/SideDrawer.vue';
 
-  export default {
+export default {
     data() {
-      return {
-        msg: 'Hello love!',
-        showFeedForm: false,
-        newFeed: {
-            name: '',
-        },
-      }
+        return {
+            msg: 'Hello love!',
+        }
     },
-    computed: {
-      addFeedButtonText() {
-          if (this.showFeedForm)
-            return 'Create';
-          else
-            return 'Add feed';
-      }
-    },
-    methods: {
-      createFeed() {
-          console.log(`Creating feed: ${this.newFeed.name}`);
-          this.newFeed.name = '';
-          this.showFeedForm = false;
-      }
+    components: {
+        SideDrawer,
     }
-  }
+}
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #222b3b;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #222b3b;
+  color: #ffffff;
+}
 
-    .title {
-        text-align: left;
-        padding-left: 16;
-    }
+.title {
+  text-align: left;
+  padding-left: 16;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
-
-    .drawer-header, .drawer-item {
-        background-color: #2d394e;
-        color: #ffffff;
-    }
-
-    .drawer-header {
-        padding: 25 16 16 16;
-        /* margin-bottom: 16; */
-        font-size: 16;
-        font-weight: bold;
-        color: #AAA;
-    }
-
-    .drawer-item {
-        padding: 8 16;
-        font-size: 16;
-    }
-
-    .drawer-item.active {
-        font-weight: bold;
-    }
-
-    .show-feed-form {
-        text-align: center;
-        font-size: 20;
-        color: #FFFFFF;
-        padding: 8 16;
-    }
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
 </style>
--- app/components/SideDrawer.vue
+++ app/components/SideDrawer.vue
@@ -1,70 +1,60 @@
 <template>
-    <RadSideDrawer ref="drawer">
-        <StackLayout ~drawerContent backgroundColor="#2d394e">
-            <Label class="drawer-header" text="Feeds"/>
+<StackLayout backgroundColor="#2d394e">
+  <Label class="drawer-header" text="Feeds"/>
 
-            <Label class="drawer-item" text="Item 1"/>
-            <Label class="drawer-item" text="Item 2"/>
-            <Label class="drawer-item" text="Item 3"/>
-            <TextField v-if="showFeedForm"
-                        hint="Create feed" 
-                        color="black" 
-                        backgroundColor="white"
-                        v-model="newFeed.name" />
-            <Button v-if="!showFeedForm" text="Add feed" @tap="showFeedForm = true"></Button>
-            <Button v-else text="Create" @tap="createFeed()"></Button>
-        </StackLayout>
-
-        <GridLayout ~mainContent columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </RadSideDrawer>
+  <Label class="drawer-item" text="All Downloads"/>
+  <Label class="drawer-item" text="Item 2"/>
+  <Label class="drawer-item" text="Item 3"/>
+  <TextField v-if="showFeedForm"
+             hint="Create feed"
+             color="black"
+             backgroundColor="white"
+             v-model="newFeed.name" />
+  <Button v-if="!showFeedForm" text="Add feed" @tap="showFeedForm = true"></Button>
+  <Button v-else text="Create" @tap="createFeed()"></Button>
+</StackLayout>
 </template>
 
 <script lang="ts">
-  export default {
-    data() {
-      return {
-        showFeedForm: false,
-        newFeed: {
-            name: '',
+    export default {
+        data() {
+            return {
+                showFeedForm: false,
+                newFeed: {
+                    name: '',
+                },
+            }
         },
-      }
-    },
-    computed: {
-      addFeedButtonText() {
-          if (this.showFeedForm)
-            return 'Create';
-          else
-            return 'Add feed';
-      }
-    },
-    methods: {
-      createFeed() {
-          console.log(`Creating feed: ${this.newFeed.name}`);
-          this.newFeed.name = '';
-          this.showFeedForm = false;
-      }
+        methods: {
+            createFeed() {
+                console.log(`Creating feed: ${this.newFeed.name}`);
+                this.newFeed.name = '';
+                this.showFeedForm = false;
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-    .drawer-header, .drawer-item {
-        background-color: #2d394e;
-        color: #ffffff;
-    }
+.drawer-header, .drawer-item {
+  background-color: #2d394e;
+  color: #ffffff;
+}
 
-    .drawer-header {
-        padding: 25 16 16 16;
-        /* margin-bottom: 16; */
-        font-size: 16;
-        font-weight: bold;
-        color: #AAA;
-    }
+.drawer-header {
+  padding: 25 16 16 16;
+  /* margin-bottom: 16; */
+  font-size: 16;
+  font-weight: bold;
+  color: #AAA;
+}
 
-    .drawer-item {
-        padding: 8 16;
-        font-size: 16;
-    }
+.drawer-item {
+  padding: 8 16;
+  font-size: 16;
+}
+
+.drawer-item.active {
+  font-weight: bold;
+}
 </style>
